ac4859246692879fdaa7e1ee47bfa2e82432f0c2
Bruzsa László <lacika007@gmail.com>
2021-10-26 22:20:29 +0200

    day02

---
+++ src/main/java/day02/Matrix.java
@@ -0,0 +1,44 @@
+package day02;
+
+public class Matrix {
+
+    public boolean isZeroUnderDiagonal(int[][] matrix) {
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (matrix[i][j] != 0)
+                    return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+
+        Matrix matrix = new Matrix();
+
+        int[][] mTrue = {
+                {1, 2, 3, 4, 5},
+                {0, 2, 3, 4, 5},
+                {0, 0, 3, 4, 5},
+                {0, 0, 0, 4, 5},
+                {0, 0, 0, 0, 5},
+        };
+
+        System.out.println(matrix.isZeroUnderDiagonal(mTrue));
+
+        int[][] mFalse = {
+                {1, 2, 3, 4, 5},
+                {0, 2, 3, 4, 5},
+                {0, 0, 3, 4, 5},
+                {0, 0, 0, 4, 5},
+                {0, 0, 0, 4, 5},
+        };
+
+        System.out.println(matrix.isZeroUnderDiagonal(mFalse));
+
+    }
+
+}
