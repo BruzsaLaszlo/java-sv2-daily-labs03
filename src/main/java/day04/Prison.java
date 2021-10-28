a0bd37becb24b728ce499401f6c6d02f9d1cb95f
Bruzsa László <lacika007@gmail.com>
2021-10-28 15:51:40 +0200

    Sultan

---
+++ src/main/java/day04/Prison.java
@@ -0,0 +1,18 @@
+package day04;
+
+public class Prison {
+
+    private boolean[] cells = new boolean[100];
+
+    public void openFreeCells() {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j += i + 1) {
+                cells[j] = !cells[j];
+            }
+        }
+    }
+
+    public boolean[] getCells() {
+        return cells;
+    }
+}
